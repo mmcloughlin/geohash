fc13dd2a5f5a48dcd056549007c40bd4766afa15
Michael McLoughlin <mmcloughlin@gmail.com>
2015-07-16 23:54:04 -0700

    Decoding examples

--- example_test.go
+++ example_test.go
@@ -25,3 +25,21 @@ func ExampleEncodeWithPrecision() {
 	fmt.Println(geohash.EncodeWithPrecision(48.858, 2.294, 5))
 	// Output: u09tu
 }
+
+func ExampleDecode() {
+	lat, lng := geohash.Decode("u09tunq6")
+	fmt.Printf("%.3f %.3f\n", lat, lng)
+	// Output: 48.858 2.294
+}
+
+func ExampleDecodeInt() {
+	lat, lng := geohash.DecodeInt(0xd0139d52c6b54c69)
+	fmt.Printf("%.3f %.3f\n", lat, lng)
+	// Output: 48.858 2.294
+}
+
+func ExampleDecodeIntWithPrecision() {
+	lat, lng := geohash.DecodeIntWithPrecision(0xd013, uint(16))
+	fmt.Printf("%.3f %.3f\n", lat, lng)
+	// Output: 48.600 2.000
+}
