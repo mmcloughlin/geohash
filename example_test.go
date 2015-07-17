c2212987617613c93cdeea99cde0ec97fc500c28
Michael McLoughlin <mmcloughlin@gmail.com>
2015-07-16 23:45:31 -0700

    More examples for encode functions

--- example_test.go
+++ example_test.go
@@ -10,3 +10,18 @@ func ExampleEncode() {
 	fmt.Println(geohash.Encode(48.858, 2.294))
 	// Output: u09tunq6qp66
 }
+
+func ExampleEncodeInt() {
+	fmt.Printf("%016x\n", geohash.EncodeInt(48.858, 2.294))
+	// Output: d0139d52c6b54c69
+}
+
+func ExampleEncodeIntWithPrecision() {
+	fmt.Printf("%08x\n", geohash.EncodeIntWithPrecision(48.858, 2.294, 32))
+	// Output: d0139d52
+}
+
+func ExampleEncodeWithPrecision() {
+	fmt.Println(geohash.EncodeWithPrecision(48.858, 2.294, 5))
+	// Output: u09tu
+}
