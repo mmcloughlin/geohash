71fdd146e641f67705553200404b1bc426871d7e
Josh Baker <joshbaker77@gmail.com>
2017-08-07 13:29:19 -0700

    Improve EncodeInt performance (#5)
    ~5x speed boost to EncodeInt by precalculating math.Exp2(32)
    as a global variable.
    
    before:
    BenchmarkEncodeInt-8	20000000            79.8 ns/op
    
    after:
    BenchmarkEncodeInt-8	100000000           13.9 ns/op
    
    *On a MacBook Pro 15" 2.8 GHz Intel Core i7 using Go 1.8.*

--- benchmark_test.go
+++ benchmark_test.go
@@ -2,8 +2,27 @@ package geohash
 
 import "testing"
 
+func randomPoints(n int) [][2]float64 {
+	var points [][2]float64
+	for i := 0; i < n; i++ {
+		lat, lon := RandomPoint()
+		points = append(points, [2]float64{lat, lon})
+	}
+	return points
+}
+
 func BenchmarkEncode(b *testing.B) {
+	points := randomPoints(b.N)
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		Encode(RandomPoint())
+		Encode(points[i][0], points[i][1])
+	}
+}
+
+func BenchmarkEncodeInt(b *testing.B) {
+	points := randomPoints(b.N)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		EncodeInt(points[i][0], points[i][1])
 	}
 }
